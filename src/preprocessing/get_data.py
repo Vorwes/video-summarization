b78e6143a5c66dd89eeb0c862daa0e4dd019d71c
Vorwes <thabitbustanji@hotmail.com>
2026-05-15 21:41:24 +0300

    feat: check if video already exists in the dataset

--- src/preprocessing/get_data.py
+++ src/preprocessing/get_data.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from youtube_transcript_api import YouTubeTranscriptApi
 
@@ -23,12 +24,33 @@ def get_arabic_transcript(ytt_api: YouTubeTranscriptApi, video_id: str) -> str |
 def append_to_jsonl(video_id: str, transcript: str) -> None:
     entry = {"video_id": video_id, "transcript": transcript}
 
-    with open("dataset.jsonl", "a", encoding="utf-8") as f:
+    os.makedirs("data", exist_ok=True)
+
+    with open("data/dataset.jsonl", "a", encoding="utf-8") as f:
         f.write(json.dumps(entry, ensure_ascii=False) + "\n")
 
 
+def is_duplicate(video_id: str, file_path: str = "data/dataset.jsonl") -> bool:
+    search_string = f'"video_id": "{video_id}"'
+
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            for line in f:
+                if search_string in line:
+                    return True
+    except FileNotFoundError:
+        return False
+
+    return False
+
+
 if __name__ == "__main__":
-    video_id = "example_id"
+    video_id = "l9Rp6-YoXIs"
+
+    if is_duplicate(video_id):
+        print("Video already exists in the dataset.")
+        exit()
+
     ytt_api = YouTubeTranscriptApi()
     fetched_transcript = get_arabic_transcript(ytt_api, video_id)
 
